c242a47a85e6446d6eba596b60f2fa5ad1f1cf0e
Jørgen Kvalsvik <jokva@equinor.com>
2020-10-13 15:59:20 +0200

    Build python separately on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,10 +2,16 @@ cmake -G"Ninja" ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_PREFIX_PATH=%PREFIX% ^
       -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
+      -DBUILD_PYTHON=OFF ^
       %SRC_DIR%
 
 if errorlevel 1 exit 1
 
 cmake --build . --config Release --target install
+
+if errorlevel 1 exit 1
+
+cd python
+%PYTHON% -m pip install .
 
 if errorlevel 1 exit 1
